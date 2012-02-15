dd2d1a06d974121a59acaae7c1c8e4d43b204e10
Paul Price <price@astro.princeton.edu>
2012-02-15 11:33:24 -0500

    Reworking to use pipe_tasks and pex_config.

--- bin/processExposure.py
+++ bin/processExposure.py
@@ -5,91 +5,111 @@ import mpi4py.MPI as mpi
 import pbasf2 as pbasf
 import os
 import signal
-import time
-
-import lsst.meas.algorithms # Register measurement functions
-import lsst.pipette.runHsc as runHsc
 
 
 def sigalrm_handler(signum, frame):
     sys.stderr.write('Signal handler called with signal %s\n' % (signum))
 signal.signal(signal.SIGALRM, sigalrm_handler)
-    
-def main(instrument, rerun, lFrameId):
-    print "running inst=%s rerun=%s frames=%s" % (instrument, rerun, lFrameId)
+
+
+
+def main(instrument, rerun, frameList):
+    print "Processing inst=%s rerun=%s frames=%s" % (instrument, rerun, frameList)
     try:
-        for frameId in lFrameId:
-            ProcessExposure(instrument, rerun, frameId)
+        for frame in frameList:
+            ProcessExposure(instrument, rerun, frame)
         return 0
     except:
         pbasf.ReportError("Total catastrophic failure processing frame %s" % (frameId))
-        print "THIS ERROR SHALL NOT HAVE APPEARED."
+        print "Aborting due to errors."
         mpi.COMM_WORLD.Abort(1)
         return 1
 
-# end
 
-
-def ProcessExposure(instrument, rerun, frameId):
+def ProcessExposure(instrument, rerun, frame):
     comm = mpi.COMM_WORLD
 
     if instrument == "hsc":
-        nCCD = 100
+        numCcd = 100
     elif instrument == "suprimecam":
-        nCCD = 10
+        numCcd = 10
     else:
-        raise RuntimeError("unknown instrument: %s" % (instrument))
-
-    runHsc.doLoad(instrument=instrument)
-
-    lCcdId = range(nCCD)
+        raise RuntimeError("Unknown instrument: %s" % (instrument))
 
     # Scatter: process CCDs independently
-    processor = ProcessWorker(rerun=rerun, instrument=instrument)
-    matchListAll = pbasf.ScatterJob(comm, processor, [(frameId, ccdId) for ccdId in lCcdId], root=0)
+    worker = Worker(instrument, rerun)
+    matchLists = pbasf.ScatterJob(comm, worker.process, [dict(visit=frame, ccd=ccd) for ccd in range(numCcd)],
+                                  root=0)
 
     # Together: global WCS solution
     if comm.Get_rank() == 0:
-        resultWcs = pbasf.SafeCall(globalWcs, instrument, matchListAll)
-        if not resultWcs:
-            sys.stderr.write("no global astrometric solution!!\n")
-            resultWcs = [None] * nCCD
-        ccdIdToWcs = dict(zip(lCcdId, resultWcs))
+        wcsList = pbasf.SafeCall(globalWcs, instrument, matchLists)
+        if not wcsList:
+            sys.stderr.write("Global astrometric solution failed!\n")
+            wcsList = [None] * nCCD
 
     # Scatter with data from root: save CCDs with WCS
-    pbasf.QueryToRoot(comm, SaveWorker(processor.ccdIdToCache), lambda ccdId: ccdIdToWcs[ccdId],
-                      processor.ccdIdToCache.keys(), root=0)
+    pbasf.QueryToRoot(comm, worker.write, lambda dataId: wcsList[dataId['ccd']],
+                      processor.resultCache.keys(), root=0)
     return 0
 #end
 
-class ProcessWorker:
-    def __init__(self, rerun=None, instrument="hsc"):
-        self.rerun = rerun
-        self.instrument = instrument
-        self.ccdIdToCache = dict()
+class Worker(object):
+    """Worker to process a CCD"""
+    def __init__(self, instrument, rerun):
+        if self.instrument == "hsc":
+            import lsst.obs.hscSim as hscSim
+            self.mapper = hscSim.HscSimMapper(rerun=rerun)
+            ProcessCcdTask = hscProcessCcd.HscDc2ProcessCcdTask
+            overrides = "hsc.py"
+            # XXX override defaults
+        elif self.instrument == "suprimecam":
+            import lsst.obs.suprimcecam as suprimecam
+            self.mapper = suprimecam.SuprimeCamMapper(rerun=rerun)
+            ProcessCcdTask = hscProcessCcd.SuprimeCamProcessCcdTask
+            overrides = "suprimecam.py"
+        else:
+            raise RuntimeError("Unrecognised instrument: %s" % self.instrument)
+        self.butler = dafPersist.ButlerFactory(mapper=mapper).create()
+        self.config = ProcessCcdTask.ConfigClass()
+        self.config.load(os.path.join(os.environ['HSCPIPE_DIR'], 'config', overrides))
+        self.processor = ProcessCcdTask(config=self.config)
 
-    def __call__(self, t_frameId_ccdId):
-        ccdId = t_frameId_ccdId[1]
-        frameId = t_frameId_ccdId[0]
-        print "Started processing %d,%d on %s,%d" % (frameId, ccdId, os.uname()[1], os.getpid())
+        self.resultCache = dict() # Cache to remember results for saving
+    
+    def process(self, dataId):
+        print "Started processing %s on %s,%d" % (dataId, os.uname()[1], os.getpid())
 
-        obj = runHsc.doRun(rerun=self.rerun, instrument=self.instrument,
-                           frameId=frameId, ccdId=ccdId, doMerge=False)
+        # We will do persistence ourselves
+        self.config.doWriteIsr = False
+        self.config.doWriteCalibrate = False
+        self.config.doWritePhotometry = False
 
-        # remember the obj for when we save
-        self.ccdIdToCache[ccdId] = obj
+        try:
+            self.resultCache[dataId] = self.processor.runButler(self.butler, dataId)
+        except Exception, e:
+            sys.stderr.write("Failed to process %s: %s\n" % (dataId, e))
+            raise
 
-        print "Finished processing %d,%d on %s,%d" % (frameId, ccdId, os.uname()[1], os.getpid())
+        print "Finished processing %s on %s,%d" % (dataId, os.uname()[1], os.getpid())
+        return self.resultCache[dataId].matches
 
-        # return matchList to the root
-        return obj.matchlist
+    def write(self, dataId, wcs):
+        print "Start writing %d on %s,%d" % (dataId, os.uname()[1], os.getpid())
 
-# end def
+        try:
+            result = self.resultCache[dataId]
+            self.processor.write(self.butler, dataId, result, wcs)
+            del self.resultCache[dataId]
+        except Exception, e:
+            sys.stderr.write('Failed to write %s: %s\n' % (dataId, e))
+
+        print "Finished writing CCD %d on %s,%d" % (dataId, os.uname()[1], os.getpid())
 
 
-def globalWcs(instrument, matchListAllCcd):
+def globalWcs(instrument, matchLists):
     import hsc.meas.tansip.doTansip as tansip
-    import lsst.pipette.config as pipConfig
+    import lsst.pex.policy as pexPolicy
 
     if instrument == "hsc":
         import lsst.obs.hscSim as hscSim
@@ -100,35 +120,10 @@ def globalWcs(instrument, matchListAllCcd):
         mapper = suprimecam.SuprimecamMapper()
         policyName = "suprimecam.paf"
 
-    policyPath = os.path.join(os.getenv("PIPETTE_DIR"), "policy", policyName)
-    fullPolicy = pipConfig.configuration(policyPath)
-    policy = fullPolicy['instrumentExtras']['solveTansip'].getPolicy()
+    policy = pexPolicy.Policy(os.path.join(os.getenv("SOLVETANSIP_DIR"), "policy", policyName))
+    wcs = tansip.doTansip(matchLists, policy=policy, camera=mapper.camera)
+    return tansip.getwcsList(wcs)
 
-    wcs = tansip.doTansip(matchListAllCcd, policy=policy, camera=mapper.camera)
-    wcsList = tansip.getwcsList(wcs)
-
-    return wcsList
-#end def
-
-
-class SaveWorker:
-    def __init__(self, ccdIdToCache):
-        self.ccdIdToCache = ccdIdToCache
-
-    def __call__(self, ccdId, wcs):
-        print "Start writing CCD %d on %s,%d" % (ccdId, os.uname()[1], os.getpid())
-        import lsst.pipette.runHsc as runHsc
-
-        # process it
-        try:
-            runHsc.doMergeWcs(self.ccdIdToCache[ccdId], wcs)
-            del self.ccdIdToCache[ccdId]
-        except Exception, e:
-            sys.stderr.write('SaveWorker failed to merge for %s: %s\n' % (ccdId, e))
-
-        print "Finished writing CCD %d on %s,%d" % (ccdId, os.uname()[1], os.getpid())
-            
-#end
 
 if __name__ == "__main__":
     print "argv=", sys.argv
