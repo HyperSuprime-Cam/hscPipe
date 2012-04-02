8f13d9ce2d1a2151ac47f0cbb55ec9b7cd1872d6
Paul Price <price@astro.princeton.edu>
2012-04-02 15:18:32 -0400

    Working for serial processing of a flat-field.  Haven't tried PBS or other detrends yet.

--- bin/processDetrends.py
+++ bin/processDetrends.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import os, os.path
 import sys
 import argparse
 import mpi4py.MPI as mpi
 import pbasf2 as pbasf
 
+from lsst.pipe.base import Struct
 import hsc.pipe.base.camera as hscCamera
 import hsc.pipe.tasks.processCcd as hscProcessCcd
 import hsc.pipe.tasks.detrends as hscDetrends
@@ -15,22 +17,29 @@ def processDetrends(instrument, rerun, detrend, frameList, outName=None):
     comm = mpi.COMM_WORLD
 
     if outName is None:
-        outName = "detrend-%s-\%d.fits" % rerun
+        outName = "detrend-" + rerun + "-%d.fits"
 
     butler = hscCamera.getButler(instrument, rerun)
     numCcds = hscCamera.getNumCcds(instrument)
-    ccdList = range(numCcds)
+    ccdList = range(numCcds)# if False else range(2)
+
+    config = hscDetrends.DetrendConfig()
+    override = os.path.join(os.environ['HSCPIPE_DIR'], 'config', "%s-%s.py" % (instrument, detrend))
+    try:
+        config.load(override)
+    except Exception, e:
+        sys.stderr.write("Unable to read override file (%s): %s\n" % (override, e))
 
     # Scatter/gather: process individual detrends, get backgrounds
     worker = Worker(butler, config, detrend, frameList, outName)
-    results = pbasf.ScatterJob(comm, worker.run, ccdList, root=0)
+    backgrounds = pbasf.ScatterJob(comm, worker.process, ccdList, root=0)
 
     # Together: 
     if comm.Get_rank() == 0:
-        backgrounds = pbasf.SafeCall(worker.scales, results)
+        scales = pbasf.SafeCall(worker.scales, backgrounds)
 
     # Scatter result: combine
-    pbasf.QueryToRoot(comm, worker.combine, lambda ccd: results[ccd], ccdList, root=0)
+    pbasf.QueryToRoot(comm, worker.combine, lambda ccd: scales[ccd], ccdList, root=0)
 
 
 class Worker(object):
@@ -42,41 +51,47 @@ class Worker(object):
         self.outName = outName
         self.maskData = []
 
+    def _getDataRef(self, frame, ccd):
+        dataId = {'visit': frame, 'ccd': ccd}
+        dataRefList = [ref for ref in self.butler.subset(datasetType='raw', **dataId)]
+        assert len(dataRefList) == 1
+        return dataRefList[0]
+
     def process(self, ccd):
         backgrounds = []
-        for frame in frameList:
-            dataId = {'visit': frame, 'ccd': ccd}
-            dataRefList = [ref for ref in self.butler.subset(datasetType='raw', **dataId)]
-            assert len(dataRefList) == 1
-            dataRef = dataRefList[0]
-
+        for frame in self.frameList:
+            dataRef = self._getDataRef(frame, ccd)
+            
             try:
                 result = self.task.process.run(self.detrend, dataRef)
             except Exception, e:
-                sys.stderr.write("Failed to process %s: %s\n" % (dataId, e))
+                sys.stderr.write("Failed to process frame %d ccd %d: %s\n" % (frame, ccd, e))
                 raise
 
             if self.detrend == "mask":
                 self.maskData.append(result)
             else:
-                self.butler.put("calexp", dataId, result.exposure)
+                dataRef.put(result.exposure, "calexp")
 
             backgrounds.append(result.background)
 
         return backgrounds
 
     def scales(self, backgrounds):
-        if detrend == "flat":
+        if self.detrend == "flat":
             import numpy
-            ccdScales, expScales = self.task.scale.run(numpy.array(backgrounds))
+            result = self.task.scale.run(numpy.array(backgrounds))
+            ccdScales = result.components
+            expScales = result.exposures
         else:
             ccdScales = [None] * len(backgrounds)
             expScales = None
-        return ccdScales, expScales
+        return [Struct(ccdScale=scale, expScales=expScales) for scale in ccdScales]
 
     def combine(self, ccd, scales):
         expScales = scales.expScales
         ccdScale = scales.ccdScale
+        dataRefList = [self._getDataRef(frame, ccd) for frame in self.frameList]
 
         if self.detrend == "mask":
             footprints = [data.footprintSets for data in self.maskData]
@@ -85,7 +100,7 @@ class Worker(object):
         else:
             combined = self.task.combine.run(dataRefList, expScales=expScales, finalScale=ccdScale)
             
-        combined.writeFits(outName % ccd)
+        combined.writeFits(self.outName % ccd)
 
 
 if __name__ == "__main__":
@@ -94,7 +109,7 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--rerun", type=str, required=True, help="Rerun name")
     parser.add_argument("-d", "--detrend", type=str, choices=["bias", "dark", "flat", "fringe", "mask"],
                         help="Detrend type")
-    parser.add_argument("-o", "--out", type=str, help="Pattern for output name, with \%d for ccd number")
+    parser.add_argument("-o", "--out", type=str, help="Pattern for output name, with %%d for ccd number")
     parser.add_argument("frames", type=int, nargs="+", help="Frames to combine")
 
     args = parser.parse_args()
