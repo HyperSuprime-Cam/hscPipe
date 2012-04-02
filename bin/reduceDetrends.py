74e3ebc7fd6190226843534689db7c59be3b002e
Paul Price <price@astro.princeton.edu>
2012-04-02 18:12:23 -0400

    Fix arguments.

--- bin/reduceDetrends.py
+++ bin/reduceDetrends.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--instrument", dest="instrument", help="Instrument name", default="hsc")
     parser.add_argument("-r", "--rerun", dest="rerun", help="Rerun name", default=os.getlogin())
     parser.add_argument("-d", "--detrend", type=str, choices=["bias", "dark", "flat", "fringe", "mask"],
-                        help="Detrend type")
+                        help="Detrend type", required=True)
     parser.add_argument("-O", "--out", type=str, help="Pattern for output name, with %%d for ccd number")
     parser.add_argument("frame", nargs='*', help="Frame numbers to reduce")
     pbs, args = parser.parse_args()
@@ -26,6 +26,11 @@ if __name__ == "__main__":
         print "No frames provided to process"
         exit(1)
 
-    command = "python %s/bin/processDetrends.py --instrument %s --rerun %s --detrend %s --out %s %s" % \
-        (os.environ['HSCPIPE_DIR'], args.instrument, args.rerun, args.detrend, args.out, " ".join(args.frame))
+    command = "python %s/bin/processDetrends.py" % os.environ['HSCPIPE_DIR']
+    command += " --instrument %s" % args.instrument
+    command += " --rerun %s" % args.rerun
+    command += " --detrend %s" % args.detrend
+    if args.out is not None:
+        command += " --out %s" % args.out
+    command += " " + " ".join(args.frame)
     pbs.run(command, repeats=len(args.frame)*hscCamera.getNumCcds(args.instrument))
