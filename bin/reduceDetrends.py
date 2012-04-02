af7ba289d3fd2decc870fba03019ec9f9d0b362d
Paul Price <price@astro.princeton.edu>
2012-04-02 17:25:53 -0400

    Fix problems preventing script from running.

--- bin/reduceDetrends.py
+++ bin/reduceDetrends.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--rerun", dest="rerun", help="Rerun name", default=os.getlogin())
     parser.add_argument("-d", "--detrend", type=str, choices=["bias", "dark", "flat", "fringe", "mask"],
                         help="Detrend type")
-    parser.add_argument("-o", "--out", type=str, help="Pattern for output name, with %%d for ccd number")
+    parser.add_argument("-O", "--out", type=str, help="Pattern for output name, with %%d for ccd number")
     parser.add_argument("frame", nargs='*', help="Frame numbers to reduce")
     pbs, args = parser.parse_args()
 
@@ -26,6 +26,6 @@ if __name__ == "__main__":
         print "No frames provided to process"
         exit(1)
 
-    command = "python %s/bin/processDetrends.py --instrument %s --rerun %s --detrend %s --out %s" % \
-        (os.environ['HSCPIPE_DIR'], args.instrument, args.rerun, args.detrend, args.out " ".join(args.frame))
+    command = "python %s/bin/processDetrends.py --instrument %s --rerun %s --detrend %s --out %s %s" % \
+        (os.environ['HSCPIPE_DIR'], args.instrument, args.rerun, args.detrend, args.out, " ".join(args.frame))
     pbs.run(command, repeats=len(args.frame)*hscCamera.getNumCcds(args.instrument))
