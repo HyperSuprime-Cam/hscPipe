efc75d4117763b564bc18f98722cab02eef638bd
Jim Bosch <jbosch@astro.princeton.edu>
2012-07-29 14:36:47 +0900

    disable slow algorithms in onsite processing

--- python/hsc/pipe/tasks/onsite.py
+++ python/hsc/pipe/tasks/onsite.py
@@ -10,6 +10,14 @@ def applyOnsiteConfigOverrides(root):
     root.isr.qa.doThumbnailOss = True
     root.isr.qa.doWriteFlattened = True
     root.isr.qa.doThumbnailFlattened = True
+    # Disable these just because they're slow and we don't need them for quick-look.
+    # Could probably remove even more, but I don't know what the quick-look requires.
+    root.measurement.algorithms.names.discard("multishapelet.psf")
+    root.measurement.algorithms.names.discard("multishapelet.exp")
+    root.measurement.algorithms.names.discard("multishapelet.dev")
+    root.measurement.algorithms.names.discard("multishapelet.combo")
+    root.measurement.algorithms.names.discard("centroid.gaussian") 
+    root.measurement.slots.modelFlux = "flux.gaussian"
 
 class SubaruProcessCcdOnsiteConfig(SubaruProcessCcdTask.ConfigClass):
     qa = pexConfig.ConfigurableField(target = QaTask, doc = "QA analysis")
