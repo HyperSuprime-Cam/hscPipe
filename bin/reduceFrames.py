c0c27610e89f1aa506a2671ab0dbb49a8577a8f5
Hisanori Furusawa <furusawa@subaru.naoj.org>
2012-08-07 16:19:15 +0900

    Modfied in pBasf mode (reduceFrames.py and processExposure.py) to accept (1) explicit listing of slave node names for qsub, (2) input and output directories for data, (3) overriding user config file

--- bin/reduceFrames.py
+++ bin/reduceFrames.py
@@ -15,6 +15,9 @@ if __name__ == "__main__":
     """)
     parser.add_argument("-i", "--instrument", dest="instrument", help="Instrument name", default="hsc")
     parser.add_argument("-r", "--rerun", dest="rerun", help="Rerun name", default=os.environ["LOGNAME"])
+    parser.add_argument("-I", "--input-data-dir", dest="root", help="directory to input data (e.g., /data/Subaru/HSC)")
+    parser.add_argument("-O", "--output-data-dir", dest="outputRoot", help="directory to output data (e.g., /data/rerun/XXX)")
+    parser.add_argument("-C", "--configfile", dest="configFile", help="config override file")
     parser.add_argument("frame", nargs='*', help="Frame numbers to reduce")
     pbs, args = parser.parse_args()
 
@@ -22,6 +25,8 @@ if __name__ == "__main__":
         print "No frames provided to process"
         exit(1)
 
-    command = "python %s/bin/processExposure.py %s %s %s" % (os.environ['HSCPIPE_DIR'], args.instrument,
-                                                             args.rerun, " ".join(args.frame))
+    command = "python %s/bin/processExposure.py %s %s %s %s %s %s" % (os.environ['HSCPIPE_DIR'], args.instrument,
+                                                                   args.rerun,
+                                                                   args.root, args.outputRoot, args.configFile, 
+                                                                   " ".join(args.frame))
     pbs.run(command, repeats=len(args.frame), threads=10 if args.instrument == "suprimecam" else None)
