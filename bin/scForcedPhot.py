07d215d4811af4cc8d567effb5740b4335a19297
Jim Bosch <jbosch@astro.princeton.edu>
2012-11-12 08:49:56 -0500

    update forced photometry API to provide reference WCS

--- bin/scForcedPhot.py
+++ bin/scForcedPhot.py
@@ -1,27 +1,6 @@
 #!/usr/bin/env python
 
-import argparse, os, sys
-import hsc.pipe.tasks.forcedPhot as fp
-from hsc.pipe.base import HscArgumentParser
 
-if __name__ == "__main__":
-    parser = HscArgumentParser()
+from hsc.pipe.tasks.forcedPhot import SubaruForcedPhotTask
 
-    try:
-        namespace = parser.parse_args(config=fp.HscForcedPhotConfig())
-    except Exception, e:
-        if "--doraise" in sys.argv:
-            raise
-        print >> sys.stderr, e
-        sys.exit(1)
-
-    task = fp.HscForcedPhotTask(config=namespace.config)
-
-    for dataRef in namespace.dataRefList:
-        if namespace.doRaise:
-            task.run(dataRef)
-        else:
-            try:
-                task.run(dataRef)
-            except Exception, e:
-                task.log.log(task.log.FATAL, "Failed: %s" % e)
+SubaruForcedPhotTask.parseAndRun()
--- python/hsc/pipe/tasks/forcedPhot.py
+++ python/hsc/pipe/tasks/forcedPhot.py
@@ -13,23 +13,24 @@ import lsst.meas.algorithms as measAlg
 from lsst.pipe.tasks.forcedPhot import ForcedPhotTask, ForcedPhotConfig, ReferencesTask, ReferencesConfig
 from lsst.pex.config import Field, ConfigurableField
 
-class HscReferencesConfig(ReferencesConfig):
-    calibrate = ConfigurableField(target=ptCal.CalibrateTask, doc="Configuration for calibration of stack")
-    detection = ConfigurableField(target=measAlg.SourceDetectionTask,
-                                  doc="Configuration for detection on stack")
-    filter = Field(dtype=str, doc="Filter name of stack for references", optional=True)
+class SubaruReferencesConfig(ReferencesConfig):
+    filter = Field(dtype=str, doc="Filter name of stack for references", optional=False)
 
-class HscReferencesTask(ReferencesTask):
-    ConfigClass = HscReferencesConfig
+class SubaruReferencesTask(ReferencesTask):
+    ConfigClass = SubaruReferencesConfig
 
-    def __init__(self, *args, **kwargs):
-        super(ReferencesTask, self).__init__(*args, **kwargs)
-        self.makeSubtask("calibrate")
-        self.schema = afwTable.SourceTable.makeMinimalSchema()
-        self.algMetadata = dafBase.PropertyList()
-        self.makeSubtask("detection", schema=self.schema)
+    def getReferenceSources(self, dataRef, exposure):
+        """Get Wcs for reference sources"""
+        butler = dataRef.butlerSubset.butler
+        dataId = dataRef.dataId
+        filterName = self.config.filter if self.config.filter is not None else dataId['filter']
+        stackId = {'stack': dataId['pointing'],
+                   'patch': 999999,
+                   'filter': filterName,
+                   }
+        return butler.get("stack_src", stackId, immediate=True)
 
-    def getReferences(self, dataRef, exposure):
+    def getReferenceWcs(self, dataRef, exposure):
         """Get reference sources on (or close to) exposure"""
         butler = dataRef.butlerSubset.butler
         dataId = dataRef.dataId
@@ -38,59 +39,13 @@ class HscReferencesTask(ReferencesTask):
                    'patch': 999999,
                    'filter': filterName,
                    }
-        try:
-            sources = foilReadProxy(butler.get("stacksources", stackId))
-        except Exception, e:
-            self.log.log(self.log.INFO, "Stack products not available (%s); attempting to create" % e)
-            sources = self.measureStack(butler, stackId)
+        calexp = butler.get("stack_calexp", stackId, immediate=True)
+        return calexp.getWcs()
 
-        return sources
+class SubaruForcedPhotTask(ForcedPhotTask):
 
-    def measureStack(self, butler, dataId):
-        exposure = butler.get("stack", dataId)
-        self.interpolateNans(exposure, afwDet.createPsf("DoubleGaussian", 15, 15, 1.0))
-        calib = self.calibrate.run(exposure)
-        exposure = calib.exposure
-
-        det = self.detect(exposure)
-        sources = det.sources
-        self.measurement.run(exposure, sources, apCorr=calib.apCorr)
-        butler.put(sources, "stacksources", dataId)
-        butler.put(exposure.getPsf(), "stackpsf", dataId)
-
-        return sources
-
-
-    def interpolateNans(self, exposure, psf):
-        exposure.getMaskedImage().getMask().addMaskPlane("UNMASKEDNAN")
-        nanMasker = ipIsr.UnmaskedNanCounterF()
-        nanMasker.apply(exposure.getMaskedImage())
-        nans = ipIsr.Isr().getDefectListFromMask(exposure.getMaskedImage(), maskName='UNMASKEDNAN')
-        self.log.log(self.log.INFO, "Interpolating over %d NANs" % len(nans))
-        measAlg.interpolateOverDefects(exposure.getMaskedImage(), psf, nans, 0.0)
-
-    def detect(self, exposure):
-        table = afwTable.SourceTable.make(self.schema)
-        table.setMetadata(self.algMetadata)
-        return self.detection.makeSourceCatalog(table, exposure)
-
-    def measureExp(self, butler, dataId, stackMeas):
-        exposure = butler.get("calexp", dataId)
-        psf = butler.get("psf", dataId)
-        ccd = exposure.getDetector()
-        ccd.setTrimmed(True)
-        psf.setDetector(ccd)
-        exposure.setPsf(psf)
-        wcs = exposure.getWcs()
-        apCorr = butler.get("apCorr", dataId)
-        box = afwGeom.Box2D(exposure.getBBox())
-
-class HscForcedPhotConfig(ForcedPhotConfig):
-    references = ConfigurableField(target=HscReferencesTask, doc="Get reference objects")
-
-class HscForcedPhotTask(ForcedPhotTask):
     def readInputs(self, dataRef, *args, **kwargs):
-        struct = super(HscForcedPhotTask, self).readInputs(dataRef, *args, **kwargs)
+        struct = super(SubaruForcedPhotTask, self).readInputs(dataRef, *args, **kwargs)
         try:
             wcsExp = dataRef.get("wcs")
             struct.exposure.setWcs(wcsExp.getWcs())
@@ -98,9 +53,3 @@ class HscForcedPhotTask(ForcedPhotTask):
             self.log.log(self.log.WARN, "No WCS tweak available (%s); not updating WCS." % e)
 
         return struct
-
-
-def foilReadProxy(obj):
-    if isinstance(obj, dafPersist.ReadProxy):
-        obj = obj.__subject__
-    return obj
