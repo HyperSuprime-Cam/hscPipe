912fd2c099be3658b84f98a4f64931488b5aabf6
Paul Price <price@astro.princeton.edu>
2012-12-03 19:47:11 -0500

    Bit more careful about when to do QA, if things are turned off.

--- python/hsc/pipe/tasks/processCcd.py
+++ python/hsc/pipe/tasks/processCcd.py
@@ -47,13 +47,13 @@ class SubaruProcessCcdTask(ProcessCcdTask):
 
     def run(self, sensorRef):
         result = ProcessCcdTask.run(self, sensorRef)
-        if self.config.qa.useIcsources:
+        if self.config.qa.useIcsources and self.config.doCalibrate:
             self.qa.run(sensorRef, result.exposure, result.calib.sources)
-        else:
+        elif self.config.doMeasurement:
             self.qa.run(sensorRef, result.exposure, result.sources)
 
         if self.config.doFinalWrite:
-            self.write(sensorRef, result, wcs=result.exposure.getWcs())
+            self.write(sensorRef, result)
 
         return result
 
