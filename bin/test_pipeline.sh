f46c5af2cf41e79c979df1650c28f8f87f685d18
Paul Price <price@astro.princeton.edu>
2013-01-02 11:25:53 -0500

    Simplify test scripts.

--- bin/test_pipeline.sh
+++ bin/test_pipeline.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #   Post this job with `qsub -V $0'
-#PBS -l nodes=2:ppn=3
-#PBS -l walltime=6000
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=100
 #PBS -N TEST
 #PBS -j oe
 #PBS -W umask=02
 echo "mpiexec is at: $(which mpiexec)"
 eups list -s
 echo $PATH
-mpiexec --verbose python $HSCPIPE_DIR/bin/test.py suprimecam price 126969
+mpiexec --verbose python $HSCPIPE_DIR/bin/test.py foobar
