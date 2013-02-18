99309bdfd250965a26726bb739701387d27650a1
Paul Price <price@astro.princeton.edu>
2013-02-18 21:19:42 +0900

    Adapt reduceStack.py to recent changes.

--- bin/reduceStack.py
+++ bin/reduceStack.py
@@ -15,21 +15,20 @@ if __name__ == "__main__":
     parser.add_argument("--wcs", dest="wcs", help="WCS for stack")
     parser.add_argument("-m", "--doMatchPsf", default=False, action='store_true',
                         help="match PSFs before stacking?")
+    parser.add_argument("--output", default=".", help="Output directory for WCSes")
     parser.add_argument("field", help="Field name in butler for stack")
     parser.add_argument("filter", help="Filter name for stack")
-    pbs, args = parser.parse_args()
+    args = parser.parse_args()
     assert args.field and args.filter, "Field and filter not provided"
 
     command = "python %s/bin/stackExposures.py" % os.environ["HSCPIPE_DIR"]
     command += " --instrument=%s --rerun=%s" % (args.instrument, args.rerun)
-    if args.output == None:
-        command += " --program=%s --filter=%s --workDirRoot=%s" % (args.field, args.filter, ".")
-    else:
-        command += " --program=%s --filter=%s --workDirRoot=%s" % (args.field, args.filter, args.output)
+    command += " --program=%s --filter=%s" % (args.field, args.filter)
+    command += " --workDirRoot=%s" % (args.output if args.output is not None else ".")
     if args.doMatchPsf:
         command += " --doMatchPsf"
     if args.wcs is not None:
         command += " --destWcs=%s" % args.wcs
     command += " --pid=%d" % os.getpid()
 
-    pbs.run(command)
+    args.pbs.run(command)
