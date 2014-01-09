e8e40a6d07edd0fd3767313d82e40f02a01fabaa
Paul Price <price@astro.princeton.edu>
2014-01-09 12:48:46 -0500

    processCoadd: add output of full source match list
    This requires use of hscProcessCoadd.py instead of processCoadd.py,
    so as to replace the top-level Task (which cannot be done dynamically).

---
+++ python/hsc/pipe/tasks/processCoadd.py
@@ -0,0 +1,22 @@
+from lsst.pipe.tasks.processCoadd import ProcessCoaddTask
+import hsc.pipe.base.matches as hscMatches
+
+class SubaruProcessCoaddConfig(ProcessCoaddTask.ConfigClass):
+    doWriteDenormalizedMatches = pexConfig.Field(
+        dtype=bool, default=True,
+        doc=("Write the denormalized match table?")
+    )
+
+class SubaruProcessCoaddTask(ProcessCoaddTask):
+    ConfigClass = SubaruProcessCoaddConfig
+
+    def run(self, dataRef):
+        results = super(SubaruProcessCoaddTask, self).run(dataRef)
+        self.write(dataRef, results)
+        return results
+
+    def write(self, dataRef, struct):
+        prefix = self.config.coaddName + "Coadd_"
+        if self.config.doWriteDenormalizedMatches:
+            dataRef.put(hscMatches.matchesToCatalog(struct.matches, struct.matchMeta), prefix + "srcMatchFull")
+
