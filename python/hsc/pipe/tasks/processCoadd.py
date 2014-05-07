45ee71f596f695ffa9d9b6fc5269d80cd2858bdf
Hisanori Furusawa <furusawa.hisanori@nao.ac.jp>
2014-05-07 11:58:36 +0900

    output srcMatchFull from processCoadd task with stack.py, too

--- python/hsc/pipe/tasks/processCoadd.py
+++ python/hsc/pipe/tasks/processCoadd.py
@@ -13,6 +13,11 @@ class SubaruProcessCoaddTask(ProcessCoaddTask):
         self.write(dataRef, results)
         return results
 
+    def process(self, dataRef, exposure):
+        results = super(SubaruProcessCoaddTask, self).process(dataRef, exposure)
+        self.write(dataRef, results)
+        return results
+
     def write(self, dataRef, struct):
         prefix = self.config.coaddName + "Coadd_"
         if self.config.doWriteDenormalizedMatches:
