1e465525d764459e769b45d227274406f3ee398a
Paul Price <price@astro.princeton.edu>
2014-06-18 13:31:04 -0400

    add Task to measure focus, based on work by Miyatake (DM-491)
    focus.py is a minimally-modified version of Miyatake's focus
    code.  This is wrapped in the ProcessFocusTask, which provides
    PBS-based processing and is also used in ProcessExposureTask.

---
+++ python/hsc/pipe/tasks/focus.py
@@ -0,0 +1,218 @@
+import numpy as np
+import scipy.interpolate as spInterpolate
+
+from lsst.pex.config import Config, Field, ListField
+
+import lsst.afw.geom as afwGeom
+import lsst.afw.cameraGeom as afwCG
+import hsc.pipe.base.camera as hscCamera
+
+class FocusConfig(Config):
+    corrCoeff = ListField(dtype=float, default=[],
+                          doc="Correction polynomial coefficients: reconstructed_focus = corr(true_focus)")
+    aboveList = ListField(dtype=int, default=[], doc="Indices of CCDs above focus")
+    belowList = ListField(dtype=int, default=[], doc="Indices of CCDs below focus")
+    offset = Field(dtype=float, default=0.12, doc="Focus offset for CCDs")
+    radialBinEdges = ListField(dtype=float, default=[], doc="Radii edges for bins")
+    radialBinCenters = ListField(dtype=float, default=[], doc="Radii centers for bins")
+    doPlot = Field(dtype=bool, default=False, doc="Plot focus calculation?")
+    shape = Field(dtype=str, default="shape.sdss", doc="Measurement to use for shape")
+    pixelScale = Field(dtype=float, default=1.0, doc="Conversion factor for pixel scale --> mm")
+
+    def isFocusCcd(self, ccdId):
+        return ccdId in self.aboveList or ccdId in self.belowList
+
+    def validate(self):
+        super(FocusConfig, self).validate()
+        numRadialBins = len(self.radialBinCenters)
+        if len(self.radialBinEdges) != numRadialBins + 1:
+            raise RuntimeError("Expected %d radialBinEdges for the %d radialBinCenters" %
+                               (numRadialBins + 1, numRadialBins))
+        if len(self.aboveList) != len(self.belowList):
+            raise RuntimeError("List of CCDs above and below focus not of equal length: %d %d" %
+                               (len(self.aboveList), len(self.belowList)))
+
+
+# get solution of the corrrelation function (reconstructed_focus -> true_focus) by Newton's method
+def getCorrectedFocus(f, df, corr_coefficients, epsilon=1e-4, n=1000):
+    ff = 0.001
+    corr = np.poly1d(corr_coefficients)
+    dcorr_dtruefocus = corr.deriv()
+    i = 0
+    while i < n:
+        fff = ff - (corr(ff)-f)/dcorr_dtruefocus(ff)
+        i += 1
+        if np.abs((fff-ff)/ff) < epsilon:
+            return fff, np.abs(1./dcorr_dtruefocus(fff))*df
+        ff = fff
+    raise RuntimeError("Cannot solve for corrected focus: %s %s %s --> %d %f" %
+                       (f, df, corr_coefficients, i, (fff-ff/ff)))
+
+def getFocusOffset(ccd, config):
+    # physical offsets are +/-0.2 mm, but offsets in the hexapod coordinates are +/-0.12 mm, which is derived from ZEMAX simulations.
+    if ccd in config.aboveList:
+        return config.offset
+    if ccd in config.belowList:
+        return -1*config.offset
+    raise KeyError("CCD identifier %s not in configuration: %s %s" % (ccd, config.aboveList, config.belowList))
+
+def getDistanceFromFocus(d_icSrc, d_ccdObj, d_dims, zemaxFilename, config, plot_filename=None):
+    # set up radial bins. Bins are optimized so that the area of each bin is the same.
+    l_binr_e = config.radialBinEdges # [16600, 17380.580580580579, 17728.128128128126, 18000]
+    l_binr = config.radialBinCenters # [17112.514461756149, 17563.380665628181, 17868.148132145379]
+    l_binr_le = l_binr_e[0:-1]
+    l_binr_ue = l_binr_e[1:]
+
+    # make selection on data and get rms^2 for each bin, ccd by ccd
+    d_l_rmssq = dict() # rmssq list for radial bin, which is dictionary for each ccd
+
+    for ccd in d_icSrc:
+        # use only objects classified as PSF candidate
+        icSrc = d_icSrc[ccd][d_icSrc[ccd].get("calib.psf.candidate")]
+
+        # prepare for getting distance from center for each object
+        ccdObj = d_ccdObj[ccd]
+        x1, y1 = d_dims[ccd]
+        # getMm() currently provides a number in pixel.
+        # Note that we constructed the zemax values alpha(r), d0(r), and this r is in pixel.
+        # We should update this code when getMm() is updated.
+        u_llc, v_llc = ccdObj.getPositionFromPixel(afwGeom.PointD(0., 0.)).getMm()
+        u_lrc, v_lrc = ccdObj.getPositionFromPixel(afwGeom.PointD(x1, 0.)).getMm()
+        u_ulc, v_ulc = ccdObj.getPositionFromPixel(afwGeom.PointD(0., y1)).getMm()
+        u_urc, v_urc = ccdObj.getPositionFromPixel(afwGeom.PointD(x1, y1)).getMm()
+
+        l_r = list()
+        l_rmssq = list()
+        for _icSrc in icSrc:
+            # reject blended objects
+            if len(_icSrc.getFootprint().getPeaks()) != 1:
+#                print "reject a blended object at CCD:%s ,(%f, %f)" % (ccd, _icSrc.getX(), _icSrc.getY())
+                continue
+
+            # calculate distance from center for each objects
+            x = _icSrc.getX()
+            y = _icSrc.getY()
+
+            u_l = (u_lrc-u_llc)/x1*x+u_llc
+            u_u = (u_urc-u_ulc)/x1*x+u_ulc
+            u = (u_u-u_l)/y1*y+u_l
+
+            v_l = (v_lrc-v_llc)/x1*x+v_llc
+            v_u = (v_urc-v_ulc)/x1*x+v_ulc
+            v = (v_u-v_l)/y1*y+v_l
+            l_r.append(np.sqrt(u**2 + v**2))
+
+            # calculate rms^2
+            mom = _icSrc.get(config.shape)
+            l_rmssq.append((mom.getIxx() + mom.getIyy())*config.pixelScale**2) # convert from pixel^2 to mm^2
+
+        # calculate median rms^2 for each radial bin
+        l_r = np.array(l_r)
+        l_rmssq = np.array(l_rmssq)
+        l_rmssq_median = list()
+        for binr_le, binr_ue in zip(l_binr_le, l_binr_ue):
+            sel = np.logical_and(l_r > binr_le, l_r < binr_ue)
+            l_rmssq_median.append(np.median(l_rmssq[sel]))
+        d_l_rmssq[ccd] = np.ma.masked_array(l_rmssq_median, mask = np.isnan(l_rmssq_median))
+
+    # get ZEMAX values
+    d = np.loadtxt(zemaxFilename)
+    s_alpha = spInterpolate.InterpolatedUnivariateSpline(d[:,0], d[:,1])
+    s_d0 = spInterpolate.InterpolatedUnivariateSpline(d[:,0], d[:,2])
+
+    # calculate rms^2 for each CCD pair
+    l_ccd_pair = zip(config.belowList, config.aboveList)
+    l_l_d = list()
+    for ccd_pair in l_ccd_pair:
+        l_d = list()
+        for i_binr, binr in enumerate(l_binr):
+            rmssq_p = d_l_rmssq[ccd_pair[1]][i_binr]
+            rmssq_m = d_l_rmssq[ccd_pair[0]][i_binr]
+            rmssq_diff = rmssq_p - rmssq_m
+            delta = getFocusOffset(ccd_pair[1], config)
+            alpha = s_alpha(binr)
+            d = rmssq_diff/4./alpha/delta + s_d0(binr)
+            l_d.append(d)
+        l_l_d.append(np.array(l_d))
+
+    l_l_d = np.ma.masked_array(l_l_d, mask = np.isnan(l_l_d))
+    d_reconstructed = np.ma.median(l_l_d)
+    d_npoint = np.sum(np.invert(l_l_d.mask))
+    d_reconstructed_err = np.ma.std(l_l_d)*np.sqrt(np.pi/2.)/np.sqrt(d_npoint)
+
+    if config.doPlot == True:
+        if not plot_filename:
+            raise ValueError("no filename for focus plot")
+        import matplotlib
+        matplotlib.use("Agg")
+        import matplotlib.pyplot as plt
+
+        l_marker = ["o", "x", "d", "^", "<", ">"]
+        l_color = ["blue", "green", "red", "cyan", "magenta", "yellow"]
+        for i_ccdpair, ccd_pair in enumerate(l_ccd_pair):
+            delta_plot = np.ma.masked_array([getFocusOffset(ccd_pair[0], config),
+                                             getFocusOffset(ccd_pair[1], config)])
+            rmssq_plot = np.ma.masked_array([d_l_rmssq[ccd_pair[0]], d_l_rmssq[ccd_pair[1]]])
+            for i_binr in range(len(l_binr)):
+                plt.plot(delta_plot, rmssq_plot[:, i_binr], "%s--" % l_marker[i_ccdpair], color = l_color[i_binr])
+        plt.savefig(plot_filename)
+
+    d_corrected, d_corrected_err = getCorrectedFocus(d_reconstructed, d_reconstructed_err, config.corrCoeff)
+    return d_corrected, d_corrected_err, d_reconstructed, d_reconstructed_err
+
+
+def run(rerun, frame, doPlot = False):
+    l_ccd = [104, 105, 106, 107, 108, 109, 110, 111]
+
+    butler = hscCamera.getButler("HSC", rerun)
+    config = FocusConfig()
+    config.update(**hscFocusConfig.toDict()) # copying hscConfig
+    config.doPlot = doPlot
+    config.freeze()
+
+    # get filter, size
+    import pyfits
+    try:
+        header = pyfits.getheader(butler.get('calexp_filename', dataId = {"visit": frame, "ccd": 104})[0])
+    except IOError:
+        raise
+    filter = header["FILTER"]
+
+    # get data and exposure
+    d_icSrc = dict()
+    d_dims = dict()
+    d_ccd = dict()
+    for ccd in l_ccd:
+        try:
+            d_icSrc[ccd] = butler.get('icSrc', dataId = {"visit": int(frame), "ccd": ccd})
+        except RuntimeError:
+            raise
+        exposure = butler.get('calexp', dataId = {"visit": int(frame), "ccd": ccd})
+        d_dims[ccd] = exposure.getDimensions()
+        d_ccd[ccd] = afwCG.cast_Ccd(exposure.getDetector())
+
+    plot_filename = "focus_%s.png" % frame
+
+    if filter == "g":
+        zemax_config = 9
+    elif filter == "r":
+        zemax_config = 1
+    elif filter == "i":
+        zemax_config = 3
+    elif filter == "z":
+        zemax_config = 5
+    elif filter == "y":
+        zemax_config = 7
+    zemax_filename = os.path.join(os.environ["OBS_SUBARU_DIR"], "hsc", "zemax_config%s_0.0.dat" % zemax_config)
+
+    return getDistanceFromFocus(d_icSrc, d_ccd, d_dims, zemax_filename, config,
+                                plot_filename=plot_filename)
+
+if __name__ == "__main__":
+    import sys
+    rerun = "miyatake-focus-test"
+    frame = int(sys.argv[1])
+    doPlot = True
+    d, d_err, d_uncorr, d_uncorr_err = run(rerun, frame, doPlot = doPlot)
+    print "focus before correction: %s +/- %s" % (d_uncorr, d_uncorr_err)
+    print "focus after correction: %s +/- %s" % (d, d_err)
---
+++ python/hsc/pipe/tasks/focusTask.py
@@ -0,0 +1,180 @@
+import math
+import collections
+
+from lsst.pex.config import Config, DictField, ConfigField, ConfigurableField
+import lsst.afw.table as afwTable
+import lsst.afw.cameraGeom as afwCG
+import lsst.afw.cameraGeom.utils as afwCGU
+import lsst.meas.algorithms as measAlg
+from lsst.pipe.base import Task, Struct, ArgumentParser
+from lsst.pipe.tasks.calibrate import CalibrateTask, InitialPsfConfig
+
+from lsst.obs.subaru.isr import SubaruIsrTask
+
+from hsc.pipe.base.pool import Pool
+from hsc.pipe.base.pbs import PbsPoolTask
+from hsc.pipe.base.butler import getDataRef
+
+from .focus import FocusConfig, getDistanceFromFocus
+
+class ProcessFocusConfig(Config):
+    focus = ConfigField(dtype=FocusConfig, doc="Focus determination")
+    zemax = DictField(keytype=str, itemtype=str, default={},
+                      doc="Mapping from filter name to zemax configuration filename")
+    isr = ConfigurableField(target=SubaruIsrTask, doc="Instrument Signature Removal")
+    background = ConfigField(dtype=measAlg.estimateBackground.ConfigClass, doc="Background removal")
+    initialPsf = ConfigField(dtype=InitialPsfConfig, doc=InitialPsfConfig.__doc__)
+    detection = ConfigurableField(target=measAlg.SourceDetectionTask, doc="Source detection")
+    measurement = ConfigurableField(target=measAlg.SourceMeasurementTask, doc="Source measurement")
+    starSelector = measAlg.starSelectorRegistry.makeField("Star selection algorithm", default="secondMoment")
+
+
+class ProcessFocusTask(PbsPoolTask):
+    ConfigClass = ProcessFocusConfig
+    _DefaultName = "processFocus"
+    installInitialPsf = CalibrateTask.__dict__["installInitialPsf"]
+
+    def __init__(self, *args, **kwargs):
+        super(ProcessFocusTask, self).__init__(*args, **kwargs)
+        self.makeSubtask("isr")
+        self.schema = afwTable.SourceTable.makeMinimalSchema()
+        self.makeSubtask("detection", schema=self.schema)
+        self.makeSubtask("measurement", schema=self.schema)
+        self.starSelector = self.config.starSelector.apply()
+        self.candidateKey = self.schema.addField(
+            "calib.psf.candidate", type="Flag",
+            doc=("Flag set if the source was a candidate for PSF determination, "
+                 "as determined by the '%s' star selector.") % self.config.starSelector.name
+        )
+
+    @classmethod
+    def pbsWallTime(cls, time, parsedCmd, numNodes, numProcs):
+        config = parsedCmd.config
+        numCcds = len(config.focus.aboveList) + len(config.focus.belowList)
+        numCycles = int(math.ceil(numCcds/float(numNodes*numProcs)))
+        numExps = len(cls.RunnerClass.getTargetList(parsedCmd))
+        return time*numExps*numCycles
+
+    @classmethod
+    def _makeArgumentParser(cls, *args, **kwargs):
+        doPbs = kwargs.pop("doPbs", False)
+        parser = ArgumentParser(name="processFocus", *args, **kwargs)
+        parser.add_id_argument("--id", datasetType="raw", level="visit",
+                               help="data ID, e.g. --id visit=12345")
+        return parser
+
+    def run(self, expRef):
+        """Measure focus for exposure
+
+        This method is the top-level for running the focus measurement
+        as a stand-alone PbsPoolTask.
+
+        Only the master node runs this method.
+        """
+        pool = Pool("processFocus")
+        pool.cacheClear()
+        pool.storeSet(butler=expRef.getButler())
+
+        dataIdList = sorted([ccdRef.dataId for ccdRef in expRef.subItems("ccd") if
+                             ccdRef.datasetExists("raw") and self.isFocus(ccdRef)])
+
+        results = pool.map(self.processPool, dataIdList)
+
+        camera = expRef.get("camera")
+        plotFilename = expRef.get("focusPlot_filename")
+        focus = self.measureFocus(results, camera, plotFilename)
+        self.log.info("Focus result for %s: %s" % (expRef.dataId, focus))
+        return focus
+
+    def isFocus(self, dataRef):
+        """Is the provided dataRef for a focus CCD?"""
+        ccdId = dataRef.dataId["ccd"]
+        return self.config.focus.isFocusCcd(ccdId)
+
+    def processPool(self, cache, dataId):
+        """Process focus CCD under pool
+
+        This is a mediator for the 'process' method when running
+        under the Pool.
+
+        Only slave nodes run this method.
+
+        @param cache: Pool cache
+        @param dataId: Data identifier for CCD
+        @return Processing results (from 'process' method)
+        """
+        return self.process(getDataRef(cache.butler, dataId))
+
+    def process(self, dataRef):
+        """Process focus CCD in preparation for focus measurement
+
+        @param dataRef: Data reference for CCD
+        @return Struct(sources: source measurements,
+                       ccdId: CCD number,
+                       filterName: name of filter,
+                       dims: exposure dimensions
+                       )
+        """
+        display = False
+
+        exp = self.isr.run(dataRef).exposure
+
+        if display:
+            import lsst.afw.display.ds9 as ds9
+            ds9.mtv(exp, title="Post-ISR", frame=1)
+
+        self.installInitialPsf(exp)
+        bg, exp = measAlg.estimateBackground(exp, self.config.background, subtract=True)
+
+        if display:
+            ds9.mtv(exp, title="Post-background", frame=2)
+
+        table = afwTable.SourceTable.make(self.schema, afwTable.IdFactory.makeSimple())
+        detRet = self.detection.makeSourceCatalog(table, exp)
+        sources = detRet.sources
+        self.measurement.run(exp, sources)
+        psfCandidateList = self.starSelector.selectStars(exp, sources)
+        if psfCandidateList:
+            for cand in psfCandidateList:
+                source = cand.getSource()
+                source.set(self.candidateKey, True)
+
+        if display:
+            ds9.mtv(exp, title="Post-measurement", frame=3)
+            with ds9.Buffering():
+                for s in sources:
+                    ds9.dot("o", s.getX(), s.getY(), frame=3,
+                            ctype=ds9.GREEN if s.get("calib.psf.candidate") else ds9.RED)
+            import pdb;pdb.set_trace()
+
+        dataRef.put(sources, "icSrc")
+        filterName = exp.getFilter().getName()
+
+        return Struct(sources=sources, ccdId=dataRef.dataId["ccd"], filterName=filterName,
+                      dims=exp.getDimensions())
+
+    def measureFocus(self, resultsList, camera, plotFilename=None):
+        """Measure focus from combining individual CCDs
+
+        @param resultsList: Results of processing individual CCDs
+        @param camera: Camera object
+        @param plotFilename: Name of file for plot
+        @return tuple(corrected distance from focus,
+                      error in corrected distance from focus,
+                      uncorrected distance from focus,
+                      error in uncorrected distance from focus,
+                      )
+        """
+        resultsList = [res for res in resultsList if res is not None]
+        sources = dict((res.ccdId, res.sources) for res in resultsList)
+
+        ccdList = [afwCG.cast_Ccd(ccd) for raft in camera for ccd in afwCG.cast_Raft(raft)]
+        ccds = dict((ccd.getId().getSerial(), ccd) for ccd in ccdList)
+
+        dims = dict((res.ccdId, res.dims) for res in resultsList)
+        filterSet = set([res.filterName for res in resultsList])
+        assert len(filterSet) == 1
+        filterName = filterSet.pop()
+        zemax = self.config.zemax[filterName]
+
+        return getDistanceFromFocus(sources, ccds, dims, zemax, self.config.focus, plot_filename=plotFilename)
